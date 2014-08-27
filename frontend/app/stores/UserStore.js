512e704a923bdef4b564830e022bf144d96c292e
Joris Bontje <jorisbontje@gmail.com>
2014-08-27 20:11:24 +0200

    deposit and withdraw

--- frontend/app/stores/UserStore.js
+++ frontend/app/stores/UserStore.js
@@ -16,14 +16,18 @@ var UserStore = Fluxxor.createStore({
     },
 
     onDeposit: function(payload) {
-        // TODO deposit
         console.log("DEPOSIT", payload);
+        if (payload.amount > 0) {
+            this.user.deposit += payload.amount;
+        }
         this.emit(constants.CHANGE_EVENT);
     },
 
     onWithdraw: function(payload) {
-        // TODO withdraw
         console.log("WITHDRAW", payload);
+        if (payload.amount <= this.user.deposit) {
+            this.user.deposit -= payload.amount;
+        }
         this.emit(constants.CHANGE_EVENT);
     },
 
